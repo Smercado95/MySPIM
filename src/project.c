fe85eb5026a5cbbdd113ce2437a64bbee9e5fef3
Joshua Wood <joshwood0505@gmail.com>
2019-11-24 18:53:50 -0500

    edit 11-24-19

--- src/project.c
+++ src/project.c
@@ -36,7 +36,8 @@ int instruction_decode(unsigned op,struct_controls *controls)
 /* 5 Points */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
-
+    *data1 = Reg[r1];
+    *data2 = Reg[r2];
 }
 
 
@@ -44,14 +45,37 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
-
+    unsigned negative = offset >> 15;
+    unsigned testNum = 1;
+    
+    if (negative == testNum)
+        *extended_value = offset | 0xffff0000;
+    else
+        *extended_value = offset & 0x0000ffff;
 }
 
 /* ALU operations */
 /* 10 Points */
 int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
 {
-
+    unsigned rType = 0;
+    unsigned iType = 1;
+    
+    if (ALUSrc == iType)
+        ALU(data1, extended_value, ALUOp, ALUresult, Zero);
+    else if (ALUSrc == rType) {
+        if (funct == 0x21)
+            ALUOp = 0;
+        else if (funct == 0x23)
+            ALUOp = 1;
+        else if (funct == 0x24)
+            ALUOp = 4;
+        else if (funct == 0x2a)
+            ALUOp = 2;
+        else if (funct == 0x2b)
+            ALUOp = 3;
+    }
+    return 0;
 }
 
 /* Read / Write Memory */
